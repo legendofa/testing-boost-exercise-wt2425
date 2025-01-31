f7d1deb7350b5636ed04f3f5f765510c283eaebc
st170001 <st170001@stud.uni-stuttgart.de>
2025-01-31 14:39:10 +0100

    Reformat and test execution and build check in CI

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -15,17 +15,20 @@ struct MatrixIOFixture {
     filePath        = "example_matrix.csv";
     dimension       = 4;
     referenceMatrix = MatrixXd(4, 4);
-    referenceMatrix << 1.8923, 8.2983, 8.1238, 7.2432 - 2.3239, 1.2923, -1.2390, 7.3413 1.2324, 1.2323, 6.2342, 7.2341 5.5464, 1.1233, 1.2312, 3.2123;
+    referenceMatrix << 1.8923, 8.2983, 8.1238, 7.2432,
+        -2.3239, 1.2923, -1.2390, 7.3413,
+        1.2324, 1.2323, 6.2342, 7.2341,
+        5.5464, 1.1233, 1.2312, 3.2123;
   }
 
   std::string filePath;
   int         dimension;
   MatrixXd    referenceMatrix;
-};
+}
 
 BOOST_FIXTURE_TEST_SUITE(MatrixIOTests, MatrixIOFixture, *boost::unit_test::tolerance(1e-4))
 
-BOOST_AUTO_TEST_CASE(SaveMatrixName)
+    BOOST_AUTO_TEST_CASE(SaveMatrixName)
 {
   std::string outputFileName = "testmatrix.csv";
   matrixIO::saveData(outputFileName, referenceMatrix);
