5d859cb56eee51762d2827ddd624904c0704dfad
st170001 <st170001@stud.uni-stuttgart.de>
2025-01-31 14:29:56 +0100

    clang-format -i src/*.cpp src/*.hpp tests/*.cpp

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <sstream>
-#include "matrixIO.hpp"
 #include <Eigen/Dense>
 #include <boost/test/unit_test.hpp>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include "matrixIO.hpp"
 
 #define BOOST_TEST_DYN_LINK
 using namespace Eigen;
@@ -12,13 +12,10 @@ using namespace Eigen;
 struct MatrixIOFixture {
   MatrixIOFixture()
   {
-    filePath = "example_matrix.csv";
-    dimension = 4;
+    filePath        = "example_matrix.csv";
+    dimension       = 4;
     referenceMatrix = MatrixXd(4, 4);
-    referenceMatrix << 1.8923, 8.2983, 8.1238, 7.2432
--2.3239, 1.2923, -1.2390, 7.3413
-1.2324, 1.2323, 6.2342, 7.2341
-5.5464, 1.1233, 1.2312, 3.2123;
+    referenceMatrix << 1.8923, 8.2983, 8.1238, 7.2432 - 2.3239, 1.2923, -1.2390, 7.3413 1.2324, 1.2323, 6.2342, 7.2341 5.5464, 1.1233, 1.2312, 3.2123;
   }
 
   std::string filePath;
