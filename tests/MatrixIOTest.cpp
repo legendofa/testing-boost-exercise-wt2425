364ac336ec4c3160e1288205ea639bd840098109
st170001 <st170001@stud.uni-stuttgart.de>
2025-02-12 14:58:31 +0100

    MatrixIOTests SaveMatrixContentCheck with adapted logic to read .csv

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -46,11 +46,18 @@ BOOST_AUTO_TEST_CASE(SaveMatrixContentCheck)
   BOOST_TEST(outputFile.is_open());
 
   Eigen::MatrixXd loadedMatrix(dimension, dimension);
+  std::string line;
   for (int i = 0; i < dimension; ++i) {
+    if (!std::getline(outputFile, line)) {
+      BOOST_FAIL("Error reading line from file.");
+    }
+    std::stringstream lineStream(line);
+    std::string cell;
     for (int j = 0; j < dimension; ++j) {
-      if (!(outputFile >> loadedMatrix(i, j))) {
+      if (!std::getline(lineStream, cell, ',')) {
         BOOST_FAIL("Error reading matrix element from file.");
       }
+      loadedMatrix(i, j) = std::stod(cell);
     }
   }
   BOOST_TEST(loadedMatrix.isApprox(referenceMatrix));
